e142b35c88dc1af0b1573a604a7048d1cc5ab25a
SOHEL RANA <sohelrana104322@gmail.com>
2024-07-03 20:07:54 +0600

    Add New Folder Local Storage

---
+++ Local Storage/script.js
@@ -0,0 +1,25 @@
+
+// Theroycal: localStorage in JavaScript allows web applications to store data locally within the user's browser – with no expiration date
+
+
+// Local Storage only allow key and value pair in function & it only allow string variable
+
+// ==when we need to set any it in local storage then call setItem function==
+// example: setItem: ƒ setItem()
+
+// localStorage.setItem("name", "SOHEL RANA");
+
+// ==when we need to get value from local storage then call getItem function==
+// example: getItem: ƒ getItem()
+
+// const name = localStorage.getItem("name");
+// console.log(name);
+// output: SOHEL RANA
+
+// ==when we need to remove value from local storage then call removeItem function==
+// example: removeItem: ƒ removeItem()
+// localStorage.removeItem("name");
+
+// ==when we need to clear all  value from local storage then call clear function==
+// example:  clear : ƒ clear()
+// localStorage.clear("name");
