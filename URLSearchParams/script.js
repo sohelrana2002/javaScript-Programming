4915bd3c60376fa6eb8de9baff5da315b2f06de1
SOHEL RANA <sohelrana104322@gmail.com>
2024-07-04 22:19:27 +0600

    Practice in details

--- URLSearchParams/script.js
+++ URLSearchParams/script.js
@@ -1,7 +1,7 @@
-const info = "name=sohel+rana&age=23&education=B.Sc+in+CSE"
+// const info = "name=sohel+rana&age=23&education=B.Sc+in+CSE";
 
 // Theroy: URLSearchParams is an object of javaScript. It have lot of fuction to help working with search param in url 
-const searchParam = new URLSearchParams(info);
+// const searchParam = new URLSearchParams(info);
 // console.log(searchParam);
 
 
@@ -12,3 +12,33 @@ const searchParam = new URLSearchParams(info);
 // const getEducation = searchParam.get("education");
 // console.log(getEducation);
 // output: B.Sc in CSE
+
+// --pass object in URLSearchParams
+const infoObj = {
+    name: "Sohel Rana",
+    age: 23,
+    education: "B.Sc in CSE"
+};
+
+const searchParam = new URLSearchParams(infoObj);
+// console.log(searchParam.toString());
+// const getName = searchParam.get("name");
+// console.log(getName, 'name');
+// output : Sohel Rana
+// ---how set anthing in url 
+
+// --update name Sohel Rana to Rolex 
+searchParam.set("name", "Rolex");
+const getName = searchParam.get("name");
+console.log(getName);
+// output: Rolex
+
+// -- add extra attribute 
+searchParam.set("dept.", "Computer Science & Engineering");
+
+const getDept = searchParam.get("dept.");
+console.log(getDept);
+// output: Computer Science & Engineering
+
+console.log(searchParam.toString());
+// output: name=Rolex&age=23&education=B.Sc+in+CSE&dept.=Computer+Science+%26+Engineering
