71e55c6a0116523db43c261e6a5805a9b6df935a
SOHEL RANA <sohelrana104322@gmail.com>
2026-02-17 03:34:24 +0600

    reverse string in manual method

---
+++ Practice Problem/reverseString.js
@@ -0,0 +1,14 @@
+function reverseString(str) {
+  let result = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i];
+  }
+
+  return result;
+}
+
+const prompt = require("prompt-sync")();
+const str = String(prompt("Enter a string: "));
+
+console.log(reverseString(str));
