8f087b68da86a23da5d73fc4d6b7cbf95b5454e2
SOHEL RANA <sohelrana104322@gmail.com>
2026-02-17 00:22:57 +0600

    Factorial number

---
+++ Practice Problem/factorial.js
@@ -0,0 +1,12 @@
+function factorial(num) {
+  if (num < 0) return "Factorial not defined!";
+
+  if (num == 0 || num == 1) return num;
+
+  return num * factorial(num - 1);
+}
+
+const prompt = require("prompt-sync")();
+const num = Number(prompt("Enter factorial num: "));
+
+console.log(`Factorial of ${num} is: ${factorial(num)}`);
