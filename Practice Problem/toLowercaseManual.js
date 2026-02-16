777307c2dbb94cc12dd1ea86174ce686873643ae
SOHEL RANA <sohelrana104322@gmail.com>
2026-02-17 03:27:56 +0600

    convert upper to lower case in manual method

---
+++ Practice Problem/toLowercaseManual.js
@@ -0,0 +1,20 @@
+function toLowercaseManual(str) {
+  let result = "";
+
+  for (let i = 0; i < str.length; i++) {
+    let code = str.charCodeAt(i);
+
+    if (code >= 65 && code <= 96) {
+      result += String.fromCharCode(code + 32);
+    } else {
+      result += str[i];
+    }
+  }
+
+  return result;
+}
+
+const prompt = require("prompt-sync")();
+const str = String(prompt("Enter a string: "));
+
+console.log(toLowercaseManual(str));
